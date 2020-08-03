1e02907e71a014e719fa52dba37f3ccb61862b6e
Chad_Barnes <Chad.A.Barnes@outlook.com>
2020-08-03 14:44:02 -0400

    I did the lab. Worship me

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,40 @@
+class Book 
+    def initialize(title)
+        @title = title
+    end
 
+    def title (title = "nil")
+        @title 
+    end
 
+    def author= (newname)
+        @author = newname
+    end
+
+    def author
+        @author
+    end
+
+    def page_count= (pager)
+        @page_count = pager
+    end
+
+    def page_count 
+        @page_count
+    end
+
+    def genre= (gename)
+        @genre = gename
+    end
+
+    def genre 
+        @genre
+    end
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+    
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,49 @@
 # Make your shoe class here!
+class Shoe 
+
+   
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand (brand = "nil")
+        @brand 
+    end
+
+    def color= (color)
+        @color = color
+    end
+
+    def color 
+        @color
+    end 
+
+    def size= (size)
+    @size = size
+    end
+
+    def size 
+        @size
+    end 
+
+        def material= (mats)
+        @material = mats
+        end
+
+        def material 
+        @material
+        end
+
+        def condition= (stat)
+            @condition = stat
+        end
+
+        def condition
+            @condition
+        end
+
+        def cobble
+            @condition = "new"
+            puts "Your shoe is as good as new!"
+        end
+end
